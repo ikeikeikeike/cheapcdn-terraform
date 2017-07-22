d0d466923d4ca8d00ca04f8626aec06e46e268cb
ikeikeikeike <jp.ne.co.jp@gmail.com>
2017-07-23 01:30:09 +0900

    none

--- do/lb.tf
+++ do/lb.tf
@@ -31,11 +31,11 @@ resource "digitalocean_loadbalancer" "object" {
   # droplet_tag = "${digitalocean_tag.object.id}"
   droplet_ids = [
     # "${digitalocean_droplet.object1.id}",
-    "${digitalocean_droplet.object2.id}",
+    # "${digitalocean_droplet.object2.id}",
     "${digitalocean_droplet.object3.id}",
     "${digitalocean_droplet.object4.id}",
     "${digitalocean_droplet.object5.id}",
-    "${digitalocean_droplet.object6.id}",
+    # "${digitalocean_droplet.object6.id}",
     "${digitalocean_droplet.object7.id}",
     "${digitalocean_droplet.object8.id}",
     "${digitalocean_droplet.object9.id}",
